70aa17fe54b1a1c2885b5661a443d964ae7cd92e
Álvaro Ferreira Pires de Paiva <alvarofepipa@gmail.com>
2021-09-12 17:05:13 -0300

    feat: second auth step

---
+++ oauth.py
@@ -0,0 +1,48 @@
+import argparse
+from twython import Twython
+from utils.functions import get_env_variables
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Get tweets from Twitter API.')
+    parser.add_argument('-ot', '--oauth_token',
+                        default=None,
+                        type=str,
+                        help='Oauth token.')
+    parser.add_argument('-ots', '--oauth_token_secret',
+                        default=None,
+                        type=str,
+                        help='Oauth token secret.')
+    parser.add_argument('-v', '--verifier',
+                        default=None,
+                        type=str,
+                        help='Verifier PIN.')
+    parser.add_argument('-ef', '--env_file',
+                        default=None,
+                        type=str,
+                        help='Environment file with variables.')
+
+    args = parser.parse_args()
+    args_dict = dict(vars(args).items())
+    config = get_env_variables(
+        keys=[
+            'PUBLIC_KEY',
+            'SECRET_KEY',
+            'OAUTH_TOKEN',
+            'OAUTH_TOKEN_SECRET',
+            'VERIFIER',
+        ],
+        env_file=args_dict['env_file']
+    )
+
+    twitter = Twython(config['PUBLIC_KEY'],
+                      config['SECRET_KEY'],
+                      config['OAUTH_TOKEN'],
+                      config['OAUTH_TOKEN_SECRET'])
+
+    authorized_tokens = twitter.get_authorized_tokens(config['VERIFIER'])
+    print(authorized_tokens)
+
+
+if __name__ == '__main__':
+    main()
